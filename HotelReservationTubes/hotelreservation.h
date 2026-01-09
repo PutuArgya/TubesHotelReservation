d8fe2d7c6de70a3733316b0c690a30499f70e08a
PutuArgya <argyaisthebest@gmail.com>
2026-01-09 17:01:54 +0700

    Add files via upload

--- HotelReservationTubes/hotelreservation.h
+++ HotelReservationTubes/hotelreservation.h
@@ -1,21 +1,27 @@
 #ifndef HOTEL_H
 #define HOTEL_H
-
+#include <iostream>
 #include <string>
+
 using namespace std;
+
+typedef struct User *adrUser; 
+typedef struct Reservation *adrReservation;
+typedef struct Room *adrRoom; 
 
 struct Reservation {
     int roomNumber;
     int nights;
-    Reservation *next;
+    adrReservation next; 
+    adrReservation prev; 
 };
 
 struct User {
     string username;
     string password;
     bool isAdmin;
-    Reservation *firstRes;
-    User *next;
+    adrReservation firstRes;
+    adrUser next;
 };
 
 struct Room {
@@ -23,25 +29,53 @@ struct Room {
     string type;
     int price;
     bool available;
-    Room *next;
+    adrRoom next;
+};
+
+struct listUsers {
+    adrUser firstUser; 
+    adrUser lastUser;
+};
+
+struct listRooms {
+    adrRoom firstRoom; 
+    adrRoom lastRoom; 
+}; 
+
+struct listReservation {
+    adrReservation firstReserve; 
+    adrReservation lastReserve; 
 };
 
 // User
-void insertUser(string u, string p, bool admin);
-User* login(string u, string p);
+void createuserList(listUsers &L); 
+adrUser registerUser(string name, string pass, bool admin);
+void insertUser(listUsers &L, adrUser p);
+void removeUser(listUsers &L, listRooms &R, string username);
+adrUser login(string u, string p);
+bool isEmptyUser(listUsers L); 
+adrUser findUser(listUsers &L, string username);
 
 // Room
-void insertRoom(int no, string type, int price);
-void viewRooms();
-Room* findRoom(int no);
+void createRoomList(listRooms &L);
+adrRoom createRoom(int no, string type, int price); 
+void insertRoom(listRooms &L, adrRoom r);
+void deleteRoom(listRooms &L, int roomNumber);
+void viewRooms(listRooms &R);
+void viewAvailableRooms(listRooms L);
+adrRoom findRoom(int no, listRooms &L);
+bool isEmptyRoom(listRooms R); 
 
 // Reservation
-void makeReservation(User *u);
-void viewAllReservations();
+void makeReservation(listRooms &L, adrUser u);
+void cancelReservation(listRooms &L, adrUser u);
+void viewUserReservations(adrUser u);
+void viewAllReservations(listUsers L);
 
 // Menu
-void adminMenu();
-void userMenu(User *u);
+void mainMenu(listUsers &L, listRooms &R); 
+void adminMenu(listUsers &L, listRooms &R, adrUser Admin);
+void userMenu(listUsers &L, listRooms &R, adrUser User);
 
 //Mungkin nambah lagi...
 
